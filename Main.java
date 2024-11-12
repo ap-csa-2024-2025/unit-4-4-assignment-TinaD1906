4ad69cd6034791f1b6d523e39aca0f2b551e4ed2
TinaD1906 <dotina87@gmail.com>
2024-11-12 18:17:28 +0000

    urmom

--- Main.java
+++ Main.java
@@ -7,6 +7,36 @@ public class Main
     // You can test your method here
     // System.out.println(isPrime(1));
     // System.out.println(isPrime(17));
+
+    //problem 1
+    Scanner scan1 = new Scanner(system.in);
+    String sentence1 = scan1;
+    int i = 0; 
+    int numPvowel = 0;
+    while (i<sentence.length()-1)
+    {
+      if (sentence1.substring(i,i+1).equals("p"))
+      {
+        if (sentence1.substring(i+1,i+2).equals("a") || sentence1.substring(i+1,i+2).equals("e")|| sentence1.substring(i+1,i+2).equals("i")||sentence1.substring(i+1,i+2).equals("o")||sentence1.substring(i+1,i+2).equals("u"));
+        {
+          numPvowel = numPvowel + 1;
+        }
+      }
+    }
+
+    //problem 2 
+    {
+      String given = "Peter Piper picked a pack of pickled peppers.";
+      int i = 0;
+      int n = 0
+      String BadLetters = "etaio"
+      while (i<given.length)
+      {
+        if (given.substring(i,i+1) = Badletters.substring(n, n+1))
+
+      }
+
+    }
   }
 
   public static boolean isPrime(int N)
@@ -14,4 +44,6 @@ public class Main
     // TODO: Replace and write your code below
     return false;
   }
+
+
 }
